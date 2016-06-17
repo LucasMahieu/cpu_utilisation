186c06915f180804469a4342c5981bfec85cc77a
Lucas MAHIEU LM249982 <lm249982@grenx02.(none)>
2016-06-17 14:58:49 +0200

    1 version, export code in a pretty fonction, it plot the % of cpu usage , TODO : test if it is correct

--- src/main.c
+++ src/main.c
@@ -4,20 +4,20 @@
 #include <stdint.h>
 
 #define MAX_SIZE_LINE 100
-#define SAMPLING_TIME_WAIT 1
+#define SAMPLING_TIME_WAIT 3
 
 typedef struct {
-	char* cpu_num;
+	char cpu_num[5];
 	uint32_t user_time;
-	char* nice_time;
-	char* system_time;
-	char* idle_time;
-	char* iowait_time;
-	char* irq_time;
-	char* softirq_time;
-	char* steal_time;
-	char* guest_time;
-	char* guestnice_time;
+	uint32_t nice_time;
+	uint32_t system_time;
+	uint32_t idle_time;
+	uint32_t iowait_time;
+	uint32_t irq_time;
+	uint32_t softirq_time;
+	uint32_t steal_time;
+	uint32_t guest_time;
+	uint32_t guestnice_time;
 } cpu_time;
 
 cpu_time get_cpu_time(char* line){
@@ -25,44 +25,74 @@ cpu_time get_cpu_time(char* line){
 	
 	if(line == NULL) return ret;
 	
-	sscanf(line,"%s  %d %s %s %s %s %s %s %s %s %s",&ret.cpu_num, &ret.user_time,&ret.nice_time,&ret.system_time,&ret.idle_time,&ret.iowait_time,&ret.irq_time,&ret.softirq_time,&ret.steal_time,&ret.guest_time,&ret.guestnice_time);
+	sscanf(line,"%s %d %d %d %d %d %d %d %d %d %d",&ret.cpu_num, &ret.user_time,&ret.nice_time,&ret.system_time,&ret.idle_time,&ret.iowait_time,&ret.irq_time,&ret.softirq_time,&ret.steal_time,&ret.guest_time,&ret.guestnice_time);
 	return ret;
 }
 
-int main(void){
-	FILE* f = fopen("/proc/stat","r");
-	
+int get_cpu_utilization(int num){
 	static int cpu_idle=0;
 	static int cpu_idle_prev=0;
 	static int cpu_not_idle=0;
 	static int cpu_not_idle_prev=0;
 
-	cpu_time var;
-
 	char cpu_line[MAX_SIZE_LINE];
+	cpu_time time_prev, time;
+	int computed_total_not_idle=0, computed_total_idle=0;
+	int total=0, total_prev=0;
+	int i=0;
 
-	if(fgets(cpu_line, MAX_SIZE_LINE, f) == NULL){
-		printf("Error to get line in /proc/stat");
-		return -1;
+	// capture the /proc/stat ligne corresponding to CPU(num)
+	FILE* f = fopen("/proc/stat","r");
+	for(i=-1;i<num;++i){
+		if(fgets(cpu_line, MAX_SIZE_LINE, f) == NULL){
+			printf("Error to get line in /proc/stat");
+			return -1;
+		}
 	}
-	
-	var = get_cpu_time(cpu_line);
+	// Parse the ligne to fill the structure of time
+	time_prev = get_cpu_time(cpu_line);
+	// Compute the idle and NotIdle time
+	cpu_not_idle_prev = time_prev.user_time + time_prev.nice_time + time_prev.system_time + time_prev.irq_time + time_prev.softirq_time + time_prev.steal_time + time_prev.guestnice_time + time_prev.guest_time;
+	cpu_idle_prev = time_prev.idle_time + time_prev.iowait_time;
+	total_prev = cpu_idle_prev + cpu_not_idle_prev;
 
-	printf("cpu user time  = %d\n",var.user_time);
-	printf("line = %s",cpu_line);
-
+	// Waiting SAMPLING_TIME_WAIT to make an other measure
 	sleep(SAMPLING_TIME_WAIT);
 
-	if(fgets(cpu_line, MAX_SIZE_LINE, f) == NULL){
-		printf("Error to get line in /proc/stat");
-		return -1;
+	// capture the /proc/stat ligne corresponding to CPU(num)
+	f = fopen("/proc/stat","r");
+	for(i=-1;i<num;++i){
+		if(fgets(cpu_line, MAX_SIZE_LINE, f) == NULL){
+			printf("Error to get line in /proc/stat");
+			return -1;
+		}
 	}
+	// Parse the ligne to fill the structure of time
+	time = get_cpu_time(cpu_line);
+	// Compute the idle and NotIdle time
+	cpu_not_idle = time.user_time + time.nice_time + time.system_time + time.irq_time + time.softirq_time + time.steal_time + time.guestnice_time + time.guest_time;
+	cpu_idle = time.idle_time + time.iowait_time;
+	total = cpu_idle + cpu_not_idle;
 
-	var = get_cpu_time(cpu_line);
+	// on peut alors faire la diff entre now and prev
+	computed_total_not_idle = total - total_prev;
+	computed_total_idle = cpu_idle - cpu_idle_prev;
 
-	printf("cpu time  = %d\n",var.user_time);
-	printf("line = %s",cpu_line);
+	// So, utilization = computed_total_not_idle - idle / computed_total_not_idle 
+	return (computed_total_not_idle - computed_total_idle) / computed_total_not_idle;
+}
 
+
+
+int main(void){
+	
+	int u = 0;
+	int i=0;
+	int core_num = sysconf(_SC_NPROCESSORS_ONLN);
+	for(i=-1;i<core_num;++i){
+		u = get_cpu_utilization(i);
+		printf("utilization of cpu%d=%d%\n",i,u);
+	}
 	return 0;
 }
 
